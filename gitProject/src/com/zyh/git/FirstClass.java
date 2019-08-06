52386f8d9eb69217909db8d791a6192ebdb8487a
zhangyihong55555 <zhangyihong_55555@163.com>
2019-08-06 21:12:35 +0800

    将本地工程添加到git

---
+++ gitProject/src/com/zyh/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.zyh.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello, git project1!");
+        System.out.println("hello, git project2!");
+        System.out.println("hello, git project3!");
+        System.out.println("hello, git project4!");
+        System.out.println("hello, git project5!");
+        System.out.println("hello, git project6!");
+    }
+}
